6c493e9d978f4c9581c4d733be0b813d67daa529
Jeú Chaves <jeuchaveslima@gmail.com>
2024-12-12 11:09:27 -0400

    feat: adiciona novos componentes de visualização no dashboard e integra gráficos

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,47 @@
-import { Typography } from "@mui/material";
+import { Box, Grid2, Paper, Typography } from "@mui/material";
 import { BaseLayout } from "../../layouts/BaseLayout";
 
 export const Dashboard = () => {
   return (
     <BaseLayout>
       <Typography variant="h3">Dashboard</Typography>
+      <Box mt={4}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            gap: 2,
+            mb: 2,
+          }}
+        >
+          <Box>
+            <Typography variant="h5">Visão Geral</Typography>
+          </Box>
+        </Box>
+        <Grid2 container spacing={2}>
+          <Grid2 component={Paper} size={{ xs: 12, md: 4 }} sx={{ p: 4 }}>
+            <Typography>Total de Jobs</Typography>
+            <Typography variant="h4">10</Typography>
+          </Grid2>
+          <Grid2 component={Paper} size={{ xs: 12, md: 4 }} sx={{ p: 4 }}>
+            <Typography>Tempo médio de resolução</Typography>
+            <Typography variant="h4">10</Typography>
+          </Grid2>
+          <Grid2 component={Paper} size={{ xs: 12, md: 4 }} sx={{ p: 4 }}>
+            <Typography>Alterações (%)</Typography>
+            <Typography variant="h4">10</Typography>
+          </Grid2>
+          <Grid2 component={Paper} size={{ xs: 12, md: 6 }} sx={{ p: 4 }}>
+            <Typography>Total de Jobs</Typography>
+          </Grid2>
+          <Grid2 component={Paper} size={{ xs: 12, md: 6 }} sx={{ p: 4 }}>
+            <Typography>Total de Jobs</Typography>
+          </Grid2>
+          <Grid2 component={Paper} size={{ xs: 12 }} sx={{ p: 4 }}>
+            <Typography>Total de Jobs</Typography>
+          </Grid2>
+        </Grid2>
+      </Box>
     </BaseLayout>
   );
 };
